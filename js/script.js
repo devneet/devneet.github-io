3d0dc2223e368b018ef8f374940708a09ca31433
Devneet Mohanty <devneetmohanty07@gmail.com>
2022-01-20 08:30:46 +0530

    changes made

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
-$(window).on("load", function(){
+$(window).on("load", function () {
 
-    $(".loader .inner").fadeOut(2000,function(){
+    $(".loader .inner").fadeOut(2000, function () {
         $(".loader").fadeOut(1000)
     });
 
     $(".items").isotope({
         filter: '*',
-        animationOptions:{
-            duration:1500,
+        animationOptions: {
+            duration: 1500,
             easing: 'linear',
             queue: false
         }
@@ -16,15 +16,15 @@ $(window).on("load", function(){
 });
 
 
-$(document).ready(function(){
+$(document).ready(function () {
     $('#slides').superslides({
-        animation : 'fade',
-        play : 5000,
-        pagination : false
+        animation: 'fade',
+        play: 5000,
+        pagination: false
     });
 
-    var typed = new Typed(".typed",{
-        strings: ["RPA Consultant", "BI Practioner", "Data Science Enthusiast", "Content Creator"],
+    var typed = new Typed(".typed", {
+        strings: ["Intelligent Process Automation Consultant", "Blue Prism Community MVP - 2022", "BI Practioner", "Data Science Enthusiast", "Content Creator"],
         typeSpeed: 70,
         loop: true,
         startDelay: 1500,
@@ -32,37 +32,37 @@ $(document).ready(function(){
     });
 
     $('.owl-carousel').owlCarousel({
-        loop:true,
+        loop: true,
         items: 10,
-        responsive:{
-            0:{
-                items:1
+        responsive: {
+            0: {
+                items: 1
             },
-            480:{
-                items:2
+            480: {
+                items: 2
             },
-            768:{
-                items:3
+            768: {
+                items: 3
             },
-            938:{
-                items:4
+            938: {
+                items: 4
             }
         }
     })
 
-    
+
 
     var skillTopOffset = $('.skillSection').offset().top;
     var statsTopOffset = $('.statSection').offset().top;
     var countUpFinsised = false;
     const options = {
-          duration: 60,
-        };
-        
+        duration: 60,
+    };
 
-    $(window).scroll(function(){
 
-        if(window.pageYOffset > skillTopOffset - $(window).height()+200){
+    $(window).scroll(function () {
+
+        if (window.pageYOffset > skillTopOffset - $(window).height() + 200) {
 
             $('.chart').easyPieChart({
                 //your options goes here
@@ -71,26 +71,26 @@ $(document).ready(function(){
                 trackColor: false,
                 scaleColor: false,
                 lineWidth: 4,
-                size:152,
-                onStep: function(from, to, percent){
+                size: 152,
+                onStep: function (from, to, percent) {
                     $(this.el).find('.percent').text(Math.round(percent))
                 }
             });
 
         }
 
-        if(!countUpFinsised && window.pageYOffset > statsTopOffset - $(window).height()+200){
-            $('.counter').each(function(){
+        if (!countUpFinsised && window.pageYOffset > statsTopOffset - $(window).height() + 200) {
+            $('.counter').each(function () {
                 var element = $(this);
                 var endVal = parseInt(element.text());
 
                 let demo = new CountUp(element, endVal, options);
-                    if (!demo.error) {
-                        demo.start();
-                    } else {
-                        console.error(demo.error);
-                    }
-        
+                if (!demo.error) {
+                    demo.start();
+                } else {
+                    console.error(demo.error);
+                }
+
                 element.countup(endVal)
             })
 
@@ -101,8 +101,8 @@ $(document).ready(function(){
 
     $("[data-fancybox]").fancybox();
 
-    
-    $("#filters a").click(function(){
+
+    $("#filters a").click(function () {
 
         $("#filters .current").removeClass("current");
         $(this).addClass("current");
@@ -111,8 +111,8 @@ $(document).ready(function(){
 
         $(".items").isotope({
             filter: selector,
-            animationOptions:{
-                duration:1500,
+            animationOptions: {
+                duration: 1500,
                 easing: 'linear',
                 queue: false
             }
@@ -122,12 +122,12 @@ $(document).ready(function(){
         return false;
     });
 
-    $("#navigation li a").click(function(e){
+    $("#navigation li a").click(function (e) {
         e.preventDefault();
 
         var targetElement = $(this).attr("href");
         var targetPosition = $(targetElement).offset().top;
-        $("html, body").animate({scrollTop : targetPosition - 50},"slow");
+        $("html, body").animate({ scrollTop: targetPosition - 50 }, "slow");
     });
 
     const nav = $("#navigation");
@@ -135,15 +135,15 @@ $(document).ready(function(){
 
     $(window).on("scroll", stickyNavigation);
 
-    function stickyNavigation(){
+    function stickyNavigation() {
 
         const body = $("body");
 
-        if($(window).scrollTop() >= navTop){
-            body.css("padding-top", nav.outerHeight()+"px")
+        if ($(window).scrollTop() >= navTop) {
+            body.css("padding-top", nav.outerHeight() + "px")
             body.addClass("fixedNav");
         }
-        else{
+        else {
             body.css("padding-top", 0)
             body.removeClass("fixedNav");
         }
